80149b49eb469859a69ef6f411279fcb800606a0
kdorcas71 <43064879+kdorcas71@users.noreply.github.com>
2024-10-24 00:41:57 +0300

    Add files via upload

---
+++ test-file.py
@@ -0,0 +1,9 @@
+#! /usr/bin/env/python
+# Python Program to Get IP Address
+import socket
+hostname = socket.gethostname()
+IPAddr = socket.gethostbyname(hostname)
+
+print("Your Computer Name is:" + hostname)
+print("Your Computer IP Address is:" + IPAddr)
+
